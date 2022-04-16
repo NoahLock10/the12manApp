c76e7f8c6502ec1b6268c41b8edb4642f588770b
Noah Lockhart <noahlock@tamu.edu>
2022-04-16 11:50:08 -0500

    lots of changes

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,14 +1,16 @@
 function userLocation() {
-    // The location of Uluru
-    const texas = { lat: 30.6187, lng: -96.3365 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 13,
-      center: texas,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: texas,
-      map: map,
-    });
-  }
+  // The location of Uluru
+  const texas = { lat: 30.6187, lng: -96.3365 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: texas,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: texas,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
